1ee9af40132c620eb5cbd65db7c8cc852ae4e5c1
wonjeheo <heo_wonje@dgist.ac.kr>
2026-07-21 14:41:58 +0900

    Improve Firebase security and guest privacy

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Firebase Authentication 콘솔에서 확인한 관리자 UID를 입력하세요.
 // 여러 관리자를 허용하려면 배열에 UID를 추가하면 됩니다.
 export const ADMIN_UIDS = Object.freeze([
-  "REPLACE_WITH_YOUR_FIREBASE_AUTH_UID"
+  "XPXg4vrl5lRXEkOZVFrybWjJeka2"
 ]);
 
 export const COLLECTIONS = Object.freeze({
